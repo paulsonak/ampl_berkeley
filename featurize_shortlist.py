36a2785d8f5f4e3e57bf6f3e6e47a87223a21975
Amanda Paulson <amanda.paulson@ucsf.edu>
2023-04-19 13:59:54 -0700

    update files to work on berkeley cluster

--- featurize_shortlist.py
+++ featurize_shortlist.py
@@ -4,7 +4,7 @@ Featurize several files at once
 """
 import sys
 import os
-os.environ['OPENBLAS_NUM_THREADS'] = '1'
+# os.environ['OPENBLAS_NUM_THREADS'] = '1'
 import json
 import pandas as pd
  
@@ -13,27 +13,24 @@ from atomsci.ddm.pipeline import parameter_parser as parse
  
 def featurize_from_shortlist(shortlist_path=None):
     """
-    Featurize and split the ChEMBL hERG pIC50 dataset. Then create a config
-    file for running a hyperparameter search to model this dataset.
+    Featurize and split a list of datasets.
     """
     sl = pd.read_csv(shortlist_path)
     
     hyper_params = {
         "hyperparam": "True",
         "split_only": "True",
-        "result_dir": '/usr/workspace/atom/PARP_compounds/Datasets_and_Models/featurize_datasets_shortlist',
-        "save_results": "True",
+        "result_dir": './',
+        "save_results": "False",
         "datastore": "False",
         "system": "LC",
-        "script_dir": "/g/g16/apaulson/github_repos/AMPL/atomsci/ddm",
-        "python_path": "/g/g16/apaulson/workspace/miniconda3/envs/ampl-140/bin/python",
         "id_col": "compound_id",
         "smiles_col" : "base_rdkit_smiles",
-        "collection_name" : "interns",
+        "collection_name" : "featurize",
         'featurizer' : 'computed_descriptors',
         "num_model_tasks":1,
     }
-    for feat in ['rdkit_raw', 'moe', 'mordred_filtered']:
+    for feat in ['rdkit_raw', 'mordred_filtered']:
         hyper_params['descriptor_type']=feat
         for i, row in sl.iterrows():
             print(f"Featurizing {row.dataset_key} with {feat}.")
@@ -47,9 +44,9 @@ def featurize_from_shortlist(shortlist_path=None):
             # Featurize and split the dataset
             split_uuid = pipe.split_dataset()
 
-    #         # Delete split file to keep it cleaner
-    #         print(hyper_params['result_dir'])
-    #         os.remove(f'{hyper_params['result_dir']}/{dataset_key.replace('.csv', '_train_valid_test_scaffold_')}{split_uuid}.csv')
+            # Delete split file to keep it cleaner
+            print(hyper_params['result_dir'])
+            os.remove(f"{hyper_params['result_dir']}/{dataset_key.replace('.csv', '_train_valid_test_scaffold_')}{split_uuid}.csv")
         
 if __name__ == '__main__':
     print('shortlist file:', sys.argv[1])
--- featurize_shortlist.sh
+++ featurize_shortlist.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH -D /global/home/users/amandapaulson/data
 #SBATCH -A ic_engin296ma
 #SBATCH --export=ALL
 #SBATCH -p savio2_htc
 #SBATCH -t 60
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name="feat_add_valid"
-#SBATCH --output="/global/home/users/amandapaulson/data/featurize_datasets_shortlist/feat_add_valid.out"
-#SBATCH --error="/global/home/users/amandapaulson/data/featurize_datasets_shortlist/feat_add_valid.out"
+#SBATCH --output="./feat_add_valid.out"
+#SBATCH --error="./feat_add_valid.out"
 
+echo $3
+cd $3
 date
-python ./featurize_shortlist.py $1
+$1 $2/featurize_shortlist.py $4
