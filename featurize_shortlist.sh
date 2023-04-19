943678553a5418088ab5d294bae009e01966afe8
paulsonak <64031328+paulsonak@users.noreply.github.com>
2023-04-19 13:09:20 -0700

    update featurize_shortlist for berkeley cluster

--- featurize_shortlist.sh
+++ featurize_shortlist.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --partition=pbatch
-#SBATCH --account=asccasc
-#SBATCH --time=720
+#SBATCH -D /global/home/users/amandapaulson/data
+#SBATCH -A ic_engin296ma
 #SBATCH --export=ALL
+#SBATCH -p savio2_htc
+#SBATCH -t 60
+#SBATCH --cpus-per-task=1
 #SBATCH --job-name="feat_add_valid"
-#SBATCH --output="/usr/workspace/atom/PARP_compounds/Datasets_and_Models/featurize_datasets_shortlist/feat_add_valid.out"
-#SBATCH --error="/usr/workspace/atom/PARP_compounds/Datasets_and_Models/featurize_datasets_shortlist/feat_add_valid.out"
-source /g/g16/apaulson/workspace/miniconda3/envs/ampl-140/bin/activate
-date
-cd /usr/workspace/atom/
+#SBATCH --output="/global/home/users/amandapaulson/data/featurize_datasets_shortlist/feat_add_valid.out"
+#SBATCH --error="/global/home/users/amandapaulson/data/featurize_datasets_shortlist/feat_add_valid.out"
 
-python ./PARP_compounds/Datasets_and_Models/featurize_datasets_shortlist/PARP_featurize_add_valid_dsets.py ./PARP_compounds/Datasets_and_Models/featurize_datasets_shortlist/PARP_shortlist_CGUAgg_2022-06.csv
+date
+python ./featurize_shortlist.py $1
